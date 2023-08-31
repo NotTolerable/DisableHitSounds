45c106aba33ccb366ecfc1c46ab53954f58b638b
NotTolerable <121141694+NotTolerable@users.noreply.github.com>
2023-08-30 22:38:08 -0400

    Update DisableHitSounds.java

--- src/main/java/me/nottolerable/disablehitsounds/DisableHitSounds.java
+++ src/main/java/me/nottolerable/disablehitsounds/DisableHitSounds.java
@@ -1,31 +1,42 @@
 package me.nottolerable.disablehitsounds;
 
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DisableHitSounds extends JavaPlugin implements Listener {
 
         @Override
         public void onEnable() {
+                getLogger().info("SnowballRemovalPlugin has been enabled!");
                 getServer().getPluginManager().registerEvents(this, this);
         }
 
+        @Override
+        public void onDisable() {
+                getLogger().info("SnowballRemovalPlugin has been disabled!");
+        }
+
         @EventHandler
-        public void onPlayerInteract(PlayerInteractEvent event) {
-                ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        public void onSnowballThrow(ProjectileLaunchEvent event) {
+                if (event.getEntityType() == EntityType.SNOWBALL) {
+                        Snowball snowball = (Snowball) event.getEntity();
+                        Location snowballLocation = snowball.getLocation();
 
-                // Check if the item is a sword
-                if (item.getType() == Material.DIAMOND_SWORD || item.getType() == Material.IRON_SWORD
-                                || item.getType() == Material.GOLDEN_SWORD || item.getType() == Material.STONE_SWORD
-                                || item.getType() == Material.WOODEN_SWORD) {
-
-                        // Check if the player is using a shield
-                        if (event.getPlayer().isBlocking()) {
-                                event.setCancelled(true);
+                        if (snowballLocation.getY() > 120) {
+                                // Schedule a task to remove the snowball every 2 ticks
+                                new BukkitRunnable() {
+                                        @Override
+                                        public void run() {
+                                                snowball.remove();
+                                        }
+                                }.runTaskLater(this, 2);
                         }
                 }
         }
