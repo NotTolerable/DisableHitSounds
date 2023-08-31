57d18321166749ca86ab58994deb5b1dedcbda60
NotTolerable <121141694+NotTolerable@users.noreply.github.com>
2023-08-30 23:10:13 -0400

    Update SnowballFlightPlugin.java

--- src/main/java/me/nottolerable/disablehitsounds/SnowballFlightPlugin.java
+++ src/main/java/me/nottolerable/disablehitsounds/SnowballFlightPlugin.java
@@ -1,34 +1,45 @@
 package me.nottolerable.disablehitsounds;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class SnowballFlightPlugin extends JavaPlugin implements Listener {
 
         @Override
         public void onEnable() {
-                getLogger().info("SnowballFlightPlugin has been enabled!");
+                getLogger().info("SnowballRemovalPlugin has been enabled!");
                 getServer().getPluginManager().registerEvents(this, this);
+
+                // Schedule a task to remove snowballs every 8 ticks (0.4 seconds)
+                new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                                removeSnowballs();
+                        }
+                }.runTaskTimer(this, 0, 8);
         }
 
         @Override
         public void onDisable() {
-                getLogger().info("SnowballFlightPlugin has been disabled!");
+                getLogger().info("SnowballRemovalPlugin has been disabled!");
         }
 
         @EventHandler
         public void onProjectileHit(ProjectileHitEvent event) {
-                Projectile projectile = event.getEntity();
+                if (event.getEntityType() == EntityType.SNOWBALL) {
+                        Snowball snowball = (Snowball) event.getEntity();
+                        snowball.remove();
+                }
+        }
 
-                if (projectile instanceof Snowball) {
-                        Snowball snowball = (Snowball) projectile;
-                        if (snowball.getTicksLived() >= 8) {
-                                snowball.remove();
-                        }
+        private void removeSnowballs() {
+                for (Snowball snowball : getServer().getWorlds().get(0).getEntitiesByClass(Snowball.class)) {
+                        snowball.remove();
                 }
         }
 }
